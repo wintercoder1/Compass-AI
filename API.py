b5ccbb44ed2ed24efc00fbebfccf08474d6694ab
Steve <steve@steves-mbp.mynetworksettings.com>
2025-02-12 21:08:54 -0500

    Added CORS middleware.

--- API.py
+++ API.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import CoreLogic
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/getPoliticalLeaningWithCitation/{query_topic}")
@@ -13,10 +22,10 @@ async def getPoliticalLeaningWithCitation(query_topic, overrideCache: bool | Non
     else:
         override = False
 
-    json = CoreLogic.parseRequestAndCompleteQuery(query_topic, 
+    jsonBody = CoreLogic.parseRequestAndCompleteQuery(query_topic, 
                                                   overrideCache=override, 
                                                   withCitation=True)
-    return json
+    return jsonBody
 
 # Will not return a citation based off of sources. Won't consider the documents gathered on possible topics.
 @app.get("/getPoliticalLeaning/{query_topic}")
@@ -27,10 +36,10 @@ async def getPoliticalLeaningWithoutCitation(query_topic, overrideCache: bool | 
     else:
         override = False
 
-    json = CoreLogic.parseRequestAndCompleteQuery(query_topic, 
+    jsonBody = CoreLogic.parseRequestAndCompleteQuery(query_topic, 
                                                   overrideCache=override, 
                                                   withCitation=False)
-    return json
+    return jsonBody
 
 @app.get("/getDEIFriendlinessScore/{query_topic}")
 async def getDEIFriendlinessScore(query_topic, overrideCache: bool | None = None):
@@ -41,34 +50,36 @@ async def getDEIFriendlinessScore(query_topic, overrideCache: bool | None = None
         override = False
     print('Override: ' + str(override))
 
-    json = CoreLogic.parseRequestAndCompleteDEIQuery(query_topic, 
+    jsonBody = CoreLogic.parseRequestAndCompleteDEIQuery(query_topic, 
                                                      overrideCache=override, 
                                                      withCitation=False)
-    return json
+    return jsonBody
 
 # GPU enabled version. Local only. Currently does not fetch citations from index
 # Note we wont cache local responses as they won't cost us as much to w.e.
 @app.get("/getPoliticalLeaningWithGPU/{query_topic}")
 async def getPoliticalLeaningWithoutCitationWithGPU(query_topic):
-    json = CoreLogic.getPoliticalLeaningWithoutCitationWithGPU(query_topic=query_topic)
-    return json
+    jsonBody = CoreLogic.getPoliticalLeaningWithoutCitationWithGPU(query_topic=query_topic)
+    return jsonBody
 
 
 #
 # Cached responses.
 #
 
+
+# TODO: Implement pagination for these two endpoints.
 # Returns all previoulsy calculated political leanings.
 @app.get("/getCachedPolitcalLeanings")
 async def getCachedPolitcalLeaningsAPI():
-    json = CoreLogic.getCachedPolitcalLeaningsEntries()
-    return json
+    jsonBody = CoreLogic.getCachedPolitcalLeaningsEntries()
+    return jsonBody
 
 # Returns all previoulsy calculated DEI friendlines scores.
 @app.get("/getCachedDEIScores")
 async def getCachedDEIFriendlinessScoresAPI():
-    json = CoreLogic.getCachedDEIFriendlinessScoresEntries()
-    return json
+    jsonBody = CoreLogic.getCachedDEIFriendlinessScoresEntries()
+    return jsonBody
 
 
 # Use this ewith the uvicorn web server.
